11d931fa86cdf33713ee66e57559362aff09f5bd
Rodrigo Santana Villegas <santana840123@gmail.com>
2023-05-27 14:09:00 -0500

    Capitulo 3: A new application

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "ESTATE",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Rodrigo Santana Villegas",
+    'website': 'https://rodrigosantanavillegas.github.io/',
+    'category': 'Category',
+    'description': 'Modulo siguiendo el paso a paso Getting Started odoo16',
+    'application': True,
+    # archivos de datos siempre cargados en la instalación
+    'data': [],
+    # archivos de datos que contienen datos de demostración cargados opcionalmente
+    'demo': [
+        'demo/demo_data.xml',
+    ],
+}
